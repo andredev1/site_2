e4d9078e22813531a2540cac9abedc321d27a320
110010011001011110110 <andre@payjustnow.com>
2022-09-16 15:48:21 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <img 
-
-style={{
-    width: '100%',
-    height: '100%'
-}}
 
 
-
-      src="https://imageswebpublic.s3.eu-west-1.amazonaws.com/background.png"
-      alt="new"
-      />
-        <p>
-          contact us
-        </p>
-        <a
+      <div style={{ 
+        height: '100vh',
+        width: '100vw',
+      backgroundImage: `url("https://imageswebpublic.s3.eu-west-1.amazonaws.com/background.png")` 
+    }}>
+      <a
           className="App-link"
-          href="https://serenitycapital.io/"
+          href="mailto:sc@ark4.net"
           target="_blank"
           rel="noopener noreferrer"
         >
-          contact us
+          contact
         </a>
+    </div>
+
+        
       </header>
     </div>
   );

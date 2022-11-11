810c721d565bba4954cb1ebb4ed9976611d46bb5
andredev1 <andredev1234567@gmail.com>
2022-11-11 19:04:00 +0200

    update

--- src/components/General/ReactAbout.js
+++ src/components/General/ReactAbout.js
@@ -14,28 +14,40 @@ class ReactAbout extends Component {
 
   render () {
     const challenges = [
-      'Lorem ipsum dolor sit amet',
-      'Lorem ipsum dolor sit amet',
-      'Lorem ipsum dolor sit amet',
-      'Lorem ipsum dolor sit amet',
-      'Lorem ipsum dolor sit amet'
+      'Organizing large amounts of data from third party API.',
+      'Complex routing within nested components.',
+      'Restructuring data from API calls into a suitable format for UI components.',
+      'Creating maintainable structure for components and resusable configurations.',
+      'Learning to use a brand new UI framework (Ant Design UI).'
     ]
 
     return (
       <Layout style={{ padding: '1rem' }}>
         <Content className="text-focus-in" style={{ ...contentStyle }}>
-          <Title level={2}>About Ark4</Title>
-          <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.  </Paragraph>
+          <Title level={2}>About Page</Title>
+          <Paragraph>Consolidating RESTFUL calls from the CoinGecko API into a React application. I&apos;ll be using v3 of the API (the most recent version at the time of starting this project) This app will be using ant design for UI. </Paragraph>
 
           <Title level={3}>Purpose</Title>
-          <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </Paragraph>
+          <Paragraph>This project was built in order to further learning and development of React applications, especially those involving connecting to third party APIs.</Paragraph>
 
           <Title level={3}>Data source</Title>
-          <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </Paragraph>
+          <Paragraph>All the data which this application ingests and displays is processed from the CoinGecko API. A free and publicly available cryptocurrency api. Click below to visit CoinGecko official website.</Paragraph>
+
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.coingecko.com/en/api#">
+            <img
+              className="responsive-image"
+              alt="coingecko api link"
+              style={{ border: '1px solid #f3f3f3', maxWidth: '400px' }}
+              src={coinGeckoLogo}>
+            </img>
+          </a>
           <Paragraph/>
 
           <Title level={3}>Challenges</Title>
-          <Paragraph>Building Ark4 posed some challenges;</Paragraph>
+          <Paragraph>Building this application posed some challenges;</Paragraph>
           <List
             style={{ marginBottom: '2rem' }}
             size="medium"
